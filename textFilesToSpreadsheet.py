8ec113e733b154c255ad9293f6d5939473a2aef5
anon <anon@anon.com>
2024-07-13 23:20:28 +0200

    Final result - working script.

--- textFilesToSpreadsheet.py
+++ textFilesToSpreadsheet.py
@@ -1,10 +1,8 @@
 import openpyxl, os
+from openpyxl.utils import get_column_letter
+
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-sonnetFile = open('sonnet29.txt')
-
-txtList = sonnetContent = sonnetFile.readlines()
 
 
 txtToExcelWB = openpyxl.Workbook()
@@ -12,9 +10,14 @@ txtToExcelWB = openpyxl.Workbook()
 sheet = txtToExcelWB.active
 
 
-for i, line in enumerate(txtList, start = 1): # By default it started from 0. Therefore we need to start from 1 since it's a spreadsheet.
-    # print(str(i) + ': ' +  line)
-    # print(type(i))
-    sheet['A' + str(i)] = line
+for folder, subfolders, txtFiles in os.walk('txtFiles'):
+    for index, txtFile in enumerate(txtFiles, start = 1):
+        oTxtFile = open(os.path.join(folder, txtFile))
+        txtList = oTxtFile.readlines()
+        
+        for i, line in enumerate(txtList, start = 1): # By default it started from 0. Therefore we need to start from 1 since it's a spreadsheet.
+            sheet[get_column_letter(index) + str(i)] = line
+        
+    
 
 txtToExcelWB.save('resultSpreadsheetFromTxtFiles.xlsx')
